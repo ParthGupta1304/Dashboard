1a0578d5dc3946ed11cce2fbfc566d1ff1fcc4a7
Parth Gupta <guptaparth1304@gmail.com>
2025-07-17 14:17:20 +0530

    final commit

--- admin-panel-frontend/src/Components/test.js
+++
@@ -1,7 +0,0 @@
-const date = new Date().toDateString();
-const time = new Date().toLocaleTimeString();
-console.log(date);
-console.log(time);
-
-console.log(typeof date);
-console.log(typeof time);
